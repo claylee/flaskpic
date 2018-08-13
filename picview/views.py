d2eb49f6306410d0dad0bbbd00b0f720e297f24f
Henry <16773293@qq.com>
2018-08-13 19:12:32 +0800

    add justifyGallery

--- picview/views.py
+++ picview/views.py
@@ -18,7 +18,7 @@ def docs(cateName = None, page = 0, pagesize=20):
     doclist = paginations.items
     pages = paginations.pages
     total = paginations.total
-    
+
     for l in doclist:
         #print(l.title)
         l.cover = docCover(l.title)
@@ -26,22 +26,17 @@ def docs(cateName = None, page = 0, pagesize=20):
     return render_template("/picview/docs.html",docs = doclist, pages = pages)
 
 @picview.route("/files/",methods=["GET","POST"])
-@picview.route("/files/<docName>/<page>/",methods=["GET","POST"])
+@picview.route("/files/<docName>/",methods=["GET","POST"])
 def files(docName = None, page = 0, pagesize = 5):
     page = int(page)
     if(page == None or page == 0):
         page = 1
 
-    paginations = picfile.query.filter().paginate(page, pagesize,error_out=False)
-    doclist = paginations.items
-    pages = paginations.pages
-    total = paginations.total
-
-    return render_template("/picview/file.html",docs = doclist, pages = pages)
+    filelist = picfile.query.filter(picfile.pictitle == docName).all()
+    return render_template("/picview/files.html",files = filelist)
 
 @picview.route("/docCover/<docname>/",methods=["GET","POST"])
 def docCover(docname = None):
     pics = picfile.query.filter(picfile.pictitle == docname).all()
-    
+
     return pics[len(pics)-1].picurl
-    
